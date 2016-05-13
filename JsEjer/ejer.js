d58d786f3a69a78a8296a40a96e0bebbc26d0792
escuelitaa <escuelitaa>
2016-05-13 14:20:30 -0300

    Subida de Server y ejercicioJS

---
+++ JsEjer/ejer.js
@@ -0,0 +1,69 @@
+
+//suma con el for
+function sumF(){ 
+		
+
+
+
+		var vectornum =[1,2,3,4];
+		var sum;
+		sum=0;
+
+
+		for (var i = 0; i < vectornum.length; i++) 
+			{
+			sum += vectornum [i];}  
+			console.log("el resultado del for es :",sum);
+			
+	};
+
+
+
+//suma con el while
+function sumW(){
+	var vectornum =[1,2,3,4,5,6];
+		var sum;
+		var i;
+		sum=0;
+		i=0;
+
+		while (i<vectornum.length) 
+			{
+				sum += vectornum [i];
+				i++;
+			}  
+			console.log("el resultado del while es :",sum);
+			
+	};
+//suma con recursion
+	function recu(){
+			
+
+			if (i == vectornum.length){
+				return res;}
+			else
+				{
+
+			 	res += vectornum[i];	
+			 	
+			 	recu(vectornum,i++,res)
+				}
+				console.log("el resultado de la recursion es :",res);	
+
+
+
+
+
+	};
+	var vectornum =[1,2,3,4,5];
+	var i=0;
+	var res=0;
+
+
+sumF(); //Llamo a la funcion sumaFor
+sumW(); //Llamo a la funcion sumaWhile
+recu();	//Llamo a la funcion sumaRecurcion
+
+
+
+
