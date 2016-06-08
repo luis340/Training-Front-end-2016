c1a9557b40a57caf34652ffe8ab0efdce33c6b63
escuelitaa <escuelitaa>
2016-06-08 16:02:32 -0300

    mejora

--- algo/public/js/nuevo.js
+++ algo/public/js/nuevo.js
@@ -3,6 +3,19 @@ var miClosure = (function() {
 	$('#bot').on('click',addUser);
 	$('#lis').on('click',listar);
 
+/*	function modify(e,data){
+		e.preventDefault();
+		console.log('entro');
+		var iN = $(this).attr('data-id');
+
+		$.ajax({
+			url:'https://connectedin.herokuapp.com/person:'+data[i]._id,
+			method:'PUT'
+
+		});
+
+	}*/
+
 
 	function listar(e){
 		e.preventDefault();
@@ -12,27 +25,44 @@ var miClosure = (function() {
 			url:'https://connectedin.herokuapp.com/person',
 			method:'GET',
 			success:function(data){
-				var myDiv='<table>';
+				var myDiv='<table id="tablaM">'+'<tr>'+
+						  '<th> ID </th>'+
+						  '<th> firstName </th>'+
+						  '<th> lastName </th>'+
+						  '<th> gender </th>'+
+						  '<th> birthday </th>'+
+						  '<th> address </th>'+
+						  '<th> photo </th>'+
+						  '<th> email </th>'+
+						  '<th> password </th>'+
+						  '<th> education </th>'+
+						  '<th> experience </th>'+
+						  '<th> Modificar </th>'+'</tr>';
+				$('#lista').append(myDiv);		  
 				for(var i=0; i<data.length; i++){
-					var remplazo= '<tr>'+'<td>'+data[i]._id+'</td>'+
-					'<td>'+data[i].firstName+'</td>'+
-					'<td>'+data[i].lastName+'</td>'+
-					'<td>'+data[i].gender+'</td>'+
-					'<td>'+data[i].birthday+'</td>'+
-					'<td>'+data[i].address+'</td>'+
-					'<td>'+data[i].photo+'</td>'+
-					'<td>'+data[i].email+'</td>'+
-					'<td>'+data[i].password+'</td>'+
-					'<td>'+data[i].education+'</td>'+
-					'<td>'+data[i].experience+'</td>'+'<tr>'; 
-								 
-								
-								 
-					myDiv +=remplazo;
-					
+					var remplazo = '<tr>'+'<td>'+data[i]._id+'</td>'+
+								   '<td>'+data[i].firstName+'</td>'+
+								   '<td>'+data[i].lastName+'</td>'+
+								   '<td>'+data[i].gender+'</td>'+
+								   '<td>'+data[i].birthday+'</td>'+
+								   '<td>'+data[i].address+'</td>'+
+								   '<td>'+data[i].photo+'</td>'+
+								   '<td>'+data[i].email+'</td>'+
+								   '<td>'+data[i].password+'</td>'+
+								   '<td>'+data[i].education+'</td>'+
+								   '<td>'+data[i].experience+'</td>'+
+								   '<td>'+'<button id="'+i+'" class="btn btn-danger">'+'Modificar'+'</button>'+'</tr>'; 
+					$('#tablaM').append(remplazo);
+
+					$('#'+i).attr('data-id',i).on('click',function(){
+						var iN = $(this).attr('data-id');
+						console.log(iN);
+					});
+							
 				}// cierra el for
-				myDiv+='</table>';
-				$('#lista').append(myDiv);
+				
+				$('#lista').append('</table>');
+								 
 				
 			}
 			
